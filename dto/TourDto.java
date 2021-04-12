df34a77ece4eb87b76cfc51cc385e2d9620f4cea
hoseokjang <72066274+hoseokjang@users.noreply.github.com>
2021-04-13 08:54:24 +0900

    Add files via upload

---
+++ dto/TourDto.java
@@ -0,0 +1,57 @@
+package pension.dto;
+
+public class TourDto {
+	private int id,readnum;
+	private String userid,title,content,fname,writeday;
+	private String name; // Member 테이블의 값을 읽어올 때 사용
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getReadnum() {
+		return readnum;
+	}
+	public void setReadnum(int readnum) {
+		this.readnum = readnum;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getWriteday() {
+		return writeday;
+	}
+	public void setWriteday(String writeday) {
+		this.writeday = writeday;
+	}
+	
+	
+}
