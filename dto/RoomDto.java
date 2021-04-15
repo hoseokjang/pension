40b0b4a27dadddf5b01cc6f703b39ef23eeaa900
hoseokjang <72066274+hoseokjang@users.noreply.github.com>
2021-04-15 12:24:04 +0900

    Add files via upload

---
+++ dto/RoomDto.java
@@ -0,0 +1,36 @@
+package pension.dto;
+
+public class RoomDto {
+	private String name;
+	private int id, min,max,price;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMin() {
+		return min;
+	}
+	public void setMin(int min) {
+		this.min = min;
+	}
+	public int getMax() {
+		return max;
+	}
+	public void setMax(int max) {
+		this.max = max;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
